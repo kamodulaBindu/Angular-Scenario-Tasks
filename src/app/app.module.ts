852357a5720dd5881117a8deb27750aaf6e2b2ce
bindu <kamodulab@gmail.com>
2019-08-07 11:02:09 +0530

    Angular-Scenario2 Task completed

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
+import { S2Component1Component } from './Scenario2/s2-component1/s2-component1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    S2Component1Component,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
